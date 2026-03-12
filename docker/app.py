bfc9fe855ec6de6fe75d3803e28fb02dde4aca8b
subhik5 <kaushikkomal334@gmail.com>
2026-03-12 14:34:12 +0530

    docker modify

---
+++ docker/app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello universe, welcome to the wolrd of devops(Thankyou)'
+
+@app.route('/health')
+def health():
+    return 'Server is up and running'
